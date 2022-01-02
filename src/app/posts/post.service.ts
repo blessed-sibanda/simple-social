83e655e268956556c826ba18b0a2ae81043d3397
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-02 17:27:09 +0200

    implement post service

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { transformError } from '../common/common';
+import { IPost, Post } from './post';
+
+interface IPostService {
+  getPostFeed(): Observable<Post[]>;
+  createPost(text: string, file?: File): Observable<Post>;
+  deletePost(id: string): Observable<any>;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PostService {
+export class PostService implements IPostService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getPostFeed(): Observable<Post[]> {
+    return this.httpClient
+      .get<IPost[]>(`${environment.baseApiUrl}/posts/`)
+      .pipe(map(Post.BuildMany), catchError(transformError));
+  }
+
+  createPost(text: string, file?: File): Observable<Post> {
+    if (!file) {
+      return this.httpClient.post<IPost>(`${environment.baseApiUrl}/posts/`, {
+        text,
+      });
+    } else {
+      const formData: FormData = new FormData();
+      formData.append('file', file);
+      formData.append('text', text);
+
+      return this.httpClient.post<IPost>(
+        `${environment.baseApiUrl}/posts/`,
+        formData
+      );
+    }
+  }
+
+  deletePost(id: string): Observable<any> {
+    return this.httpClient.delete<any>(`${environment.baseApiUrl}/posts/${id}`);
+  }
 }
--- src/app/posts/post.ts
+++ src/app/posts/post.ts
@@ -1,2 +1,33 @@
+import { IFollow, User } from '../user/user';
+
+export interface IPost {
+  _id: string;
+  text: string;
+  photoUrl: string;
+  postedBy: IFollow;
+  createdAt: string;
+}
+
 export class Post {
+  constructor(
+    public _id = '',
+    public text = '',
+    public photoUrl = '',
+    public postedBy = new User() as IFollow,
+    public createdAt = ''
+  ) {}
+
+  static Build(post: IPost): Post {
+    return new Post(
+      post._id,
+      post.text,
+      post.photoUrl,
+      post.postedBy,
+      post.createdAt
+    );
+  }
+
+  static BuildMany(posts: IPost[]): Post[] {
+    return posts.map((p) => Post.Build(p));
+  }
 }
