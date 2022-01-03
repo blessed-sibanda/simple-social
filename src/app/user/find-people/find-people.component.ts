fc08b0eacf9504e0c20243df33602034a8caf246
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-01-03 13:08:32 +0200

    display loader

--- src/app/user/find-people/find-people.component.ts
+++ src/app/user/find-people/find-people.component.ts
@@ -14,6 +14,7 @@ import { UserService } from '../user.service';
 export class FindPeopleComponent implements OnInit {
   subs = new SubSink();
   people: User[] = [];
+  loading = true;
 
   constructor(
     private userService: UserService,
@@ -27,6 +28,7 @@ export class FindPeopleComponent implements OnInit {
       .pipe(
         tap((people) => {
           this.people = people;
+          this.loading = false;
         })
       )
       .subscribe();
